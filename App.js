ab4d15db82a76915533c306b51a76bccf8c6dbb0
Shujon Mollah <mollah@gmail.com>
2017-11-12 12:41:27 -0500

    Install Mapbox (#1)
    * Add Native Mapbox SDK Framework
    
    * Add React Native Mapbox SDK Files
    
    * Add Framework Header Search Paths
    
    * Add Run Script
    
    * Store token in dotenv file
    
    * Init MapView

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+import {MAPBOX_ACCESS_TOKEN} from 'react-native-dotenv'
+import MapboxGL from '@mapbox/react-native-mapbox-gl'
 
 export default class App extends React.Component {
+  async componentWillMount() {
+    MapboxGL.setAccessToken(MAPBOX_ACCESS_TOKEN)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <MapboxGL.MapView
+          ref={(c) => this._map = c}
+          style={styles.map}
+          centerCoordinate={[-73.970895, 40.723279]}
+          zoomLevel={10} />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch'
   },
-});
+  map: {
+    flex: 1,
+  },
+})
